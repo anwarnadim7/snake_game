841812c6b5c89d8204c7748f515835705f6f3c98
Nadim Anwar <anwarnadim3@gmail.com>
2024-09-10 09:45:06 +0530

    Tail Collision Added

--- food.py
+++ food.py
@@ -6,8 +6,7 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("blue")
+        self.color("red")
         self.speed("fastest")
         self.refresh()
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging import fatal
 from turtle import Screen
 
 from scoreboard import Scoreboard
@@ -29,8 +30,19 @@ while game_is_on:
     #Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
+    #Detect collision with tail.
+    for segment in snake.segments[1:]:
+      if snake.head.distance(segment)<10:
+            game_is_on = False
+            scoreboard.game_over()
+        #if head collision with any segment in the tail
 
 screen.exitonclick()
